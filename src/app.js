4107dcd89034e8cbea878a713861b03f9651db29
Miguel <miguelarea@gmail.com>
2025-05-14 19:28:54 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -4,38 +4,62 @@ const sustantivos = ["coche", "casa", "libro", "ordenador", "perro"];
 const extensiones = [".com", ".es", ".de", ".fr", ".gb"];
 const dominios = [];
 
-for (const pronombre of pronombres) {
-  for (const adjetivo of adjetivos) {
-    for (const sustantivo of sustantivos) {
-      for (const extension of extensiones) {
-        const dominioSinSeparador = pronombre + adjetivo + sustantivo + extension;
-        dominios.push(dominioSinSeparador);
+const tablaDominiosBody = document.querySelector('#tablaDominios tbody');
+
+function generarDominios() {
+  dominios.length = 0;
+  for (const pronombre of pronombres) {
+    for (const adjetivo of adjetivos) {
+      for (const sustantivo of sustantivos) {
+        for (const extension of extensiones) {
+          const dominioSinSeparador = pronombre + adjetivo + sustantivo + extension;
+          dominios.push(dominioSinSeparador);
+        }
       }
     }
   }
+  console.log("Dominios actualizados:", dominios);
+  actualizarTabla();
 }
 
-console.log(dominios); 
+function actualizarTabla() {
+  if (tablaDominiosBody) {
+    tablaDominiosBody.innerHTML = ''; 
+    let row;
+    dominios.forEach((dominio, index) => {
+      if (index % 5 === 0) {
+        row = tablaDominiosBody.insertRow();
+      }
+      const cellDominio = row.insertCell();
+      cellDominio.textContent = dominio;
+      cellDominio.classList.add('columna');
+    });
+  }
+  console.log(`Se generaron ${dominios.length} combinaciones de dominio.`);
+}
 
-const tablaDominiosBody = document.querySelector('#tablaDominios tbody');
-if (tablaDominiosBody) {
-  let row;
-  dominios.forEach((dominio, index) => {
-    if (index % 5 === 0) {
-      row = tablaDominiosBody.insertRow();
+function agregar(tipo, inputId) {
+  const inputValue = document.getElementById(inputId).value.trim();
+  if (inputValue) {
+    switch (tipo) {
+      case 'pronombres':
+        pronombres.push(inputValue);
+        break;
+      case 'adjetivos':
+        adjetivos.push(inputValue);
+        break;
+      case 'sustantivos':
+        sustantivos.push(inputValue);
+        break;
+      case 'extensiones':
+        extensiones.push(inputValue);
+        break;
     }
-    const cellDominio = row.insertCell();
-    cellDominio.textContent = dominio;
-    cellDominio.classList.add('columna'); 
-  });
+    document.getElementById(inputId).value = ''; 
+    generarDominios(); 
+  }
 }
 
-console.log(`Se generaron ${dominios.length} combinaciones de dominio.`);
 
-function cambiarColor() {
-  const colores = ['#f00', '#0f0', '#00f', '#ff0', '#f0f', '#0ff'];
-  const colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
-  document.body.style.backgroundColor = colorAleatorio;
-}
+generarDominios();
 
-setInterval(cambiarColor, 2000);
